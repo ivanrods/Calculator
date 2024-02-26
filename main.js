621a72ea5430542aa30735376d12a4dfb4196083
ivan <contaivanrodrigues@gmail.com>
2024-02-26 11:34:15 -0300

    Add dark mode

---
+++ main.js
@@ -0,0 +1,7 @@
+let lightMode = true
+const buttonToggle = document.getElementById('toggle-mode')
+
+buttonToggle.addEventListener('click', (event) => {
+    document.documentElement.classList.toggle('dark')
+
+})
