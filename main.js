2565acef077c8d542c89fadbf1a75c347880a165
ivan <contaivanrodrigues@gmail.com>
2024-03-07 11:23:55 -0300

    add operations

--- main.js
+++ main.js
@@ -35,36 +35,66 @@ const eight = document.getElementById('eight')
 const nine = document.getElementById('nine')
 
 //events
-const numbers = []
-let arrayDeNumeros = numbers.map(function(item) {
-    return Number(item);
-});
 
+let numbers = []
+let arrayNumbers = []
+
+
+//function capture
 document.querySelectorAll("button").forEach( function(button) {
     
     button.addEventListener("click", function(event) {
     const el = event.target || event.srcElement;
     const id = el.value;
     numbers.push(id)
-    res()
-    console.log(numbers)
-    console.log(arrayDeNumeros)
+
+    arrayNumbers = numbers.map(function(item) {
+        return Number(item);
+    
+    });
+    result()
+    console.log(arrayNumbers)
   });
+  
   
 });
 
-function res(){
-    document.querySelector("h2").innerHTML = numbers.join('')
-}
-
-
-equal.addEventListener('click', ()=>{
+//function sum
+plus.addEventListener('click', ()=>{
     let sum = 0
-    for(let i = 0; i < numbers.length; i++){
-    sum+=numbers[i]
+    for(let i = 0; i < arrayNumbers.length; i++){
+    sum+=arrayNumbers[i]
    }
     document.querySelector("h1").innerHTML = sum
 })
+//funtion minus
+minus.addEventListener('click', ()=>{
+    let min = arrayNumbers[0]
+    for(let i = 1; i < arrayNumbers.length; i++) {
+        min -= arrayNumbers[i]
+    }
+    document.querySelector("h1").innerHTML = min
+})
+//function multiply
+multiply.addEventListener('click', ()=>{
+    let mult = 1
+    for(let i = 0; i < arrayNumbers.length; i++) {
+        mult*= arrayNumbers[i]
+    }
+    document.querySelector("h1").innerHTML = mult
+})
+
+//funtion result
+function result(){
+    document.querySelector("h2").innerHTML = arrayNumbers.join('')
+}
 
 
+//Function clean
+clean.addEventListener('click', ()=>{
+    
+    document.querySelector("h1").innerHTML = 0
+    document.querySelector("h2").innerHTML = 0
+    
 
+})
